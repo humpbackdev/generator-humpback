2b67bae4bbc2151ca331756bf2e51bb7767e19ff
Kevin Porras <kporras07@gmail.com>
2017-10-03 20:33:51 -0600

    Update local install script.

--- generators/app/templates/_humpback_local_install.sh
+++ generators/app/templates/_humpback_local_install.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-DRUSH="./vendor/bin/drush"
-SITE_ALIAS="@<%= dashedAppName %>.<%= dashedAppName %>.dev"
 SITE_UUID="<%= siteUuid %>"
-$DRUSH $SITE_ALIAS cc drush
+ahoy drush cc drush
 echo "Installing..."
-$DRUSH $SITE_ALIAS si <%= appName %> --account-pass=admin -y
+ahoy drush si <%= appName %> --account-pass=admin -y
 echo "Set site uuid..."
-$DRUSH $SITE_ALIAS config-set "system.site" uuid "$SITE_UUID" -y
+ahoy drush config-set "system.site" uuid "$SITE_UUID" -y
 echo "Importing config..."
-if [ -f ./config/sync/core.extension.yml ] ; then $DRUSH $SITE_ALIAS cim -y ; fi
+if [ -f ./config/sync/core.extension.yml ] ; then ahoy drush cim -y ; fi
 echo "Cleaning cache..."
-$DRUSH $SITE_ALIAS cr
+ahoy drush cr
