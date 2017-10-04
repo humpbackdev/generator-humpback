75caf3f2ca300ea2d3cd8b3329233f7ac8ad7d27
Kevin Porras <kporras07@gmail.com>
2017-10-03 21:07:02 -0600

    Update local_behat script.

--- generators/app/templates/scripts/local_behat.sh
+++ generators/app/templates/scripts/local_behat.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 if [ $# -eq 0 ]
   then
-    vagrant ssh -c "cd /var/www/<%= appName %>; ./vendor/bin/behat"
+    ahoy docker exec cli "./vendor/bin/behat"
 else
-  vagrant ssh -c "cd /var/www/<%= appName %>; ./vendor/bin/behat ./tests/behat/features/$1.feature"
+  ahoy docker exec cli "./vendor/bin/behat ./tests/behat/features/$1.feature"
+  vagrant ssh -c "cd /var/www/cecr; ./vendor/bin/behat ./tests/behat/features/$1.feature"
 fi
