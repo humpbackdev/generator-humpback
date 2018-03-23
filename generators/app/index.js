545bf21b65cced044a9b86a152ef6c5b7ec5ea64
Kevin Porras <kporras07@gmail.com>
2018-03-23 10:26:17 -0600

    Improve performance by using mysql config.

--- generators/app/index.js
+++ generators/app/index.js
@@ -49,7 +49,7 @@ module.exports = class extends Generator {
 
   writing() {
     var self = this;
-    remote('humpbackdev', 'humpback', 'v1.9', function(err, extractPath) {
+    remote('humpbackdev', 'humpback', 'v1.10', function(err, extractPath) {
       self.fs.copy(extractPath, self.destinationPath('./'));
       self.fs.copy(extractPath + '/.ahoy', self.destinationPath('.ahoy'));
       self.fs.copy(extractPath + '/.ahoy.yml', self.destinationPath('.ahoy.yml'));
@@ -61,6 +61,11 @@ module.exports = class extends Generator {
       self.fs.copyTpl(
         self.templatePath('site.ahoy.yml'),
         self.destinationPath('.ahoy/site.ahoy.yml'),
+        self.props
+      );
+      self.fs.copyTpl(
+        self.templatePath('_docker-compose.yml'),
+        self.destinationPath('docker-compose.yml'),
         self.props
       );
     });
