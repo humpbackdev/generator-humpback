0fdf66a3f58aa58593c2ddccc7fb8bfdd2b7beed
Kevin Porras <kporras07@gmail.com>
2018-04-13 10:36:57 -0500

    Get next humpback version.

--- generators/app/index.js
+++ generators/app/index.js
@@ -49,7 +49,7 @@ module.exports = class extends Generator {
 
   writing() {
     var self = this;
-    remote('humpbackdev', 'humpback', 'v1.10', function(err, extractPath) {
+    remote('humpbackdev', 'humpback', 'v1.11', function(err, extractPath) {
       self.fs.copy(extractPath, self.destinationPath('./'));
       self.fs.copy(extractPath + '/.ahoy', self.destinationPath('.ahoy'));
       self.fs.copy(extractPath + '/.ahoy.yml', self.destinationPath('.ahoy.yml'));
